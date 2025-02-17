f3268322af97914d404c047008b878f556c95aa3
caiman <dorohecaimandoro@gmail.com>
2025-02-17 19:06:15 +0900

    feat: 검색을 우측으로 옮긴다

--- src/components/TtyrecPlayer.tsx
+++ src/components/TtyrecPlayer.tsx
@@ -1,12 +1,8 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect } from "react";
 import * as AsciinemaPlayer from "asciinema-player";
 import "asciinema-player/dist/bundle/asciinema-player.css";
 import useBz2DecompressWorker from "@/hooks/useBz2DecompressWorker";
 import { States } from "@/types/decompressWorker";
-import useSearchWorker from "@/hooks/useSearchWorker";
-import { States as SearchStates } from "@/types/searchWorker";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
 
 const TtyrecPlayer = ({
   file,
@@ -16,37 +12,9 @@ const TtyrecPlayer = ({
   onEnded: () => void;
 }) => {
   const { result, status, decompressFile } = useBz2DecompressWorker();
-  const {
-    status: searchStatus,
-    result: searchResult,
-    search,
-  } = useSearchWorker();
   const containerRef = useRef<HTMLDivElement>(null);
   /* eslint-disable @typescript-eslint/no-explicit-any */
   const playerRef = useRef<any>(null);
-  const [searchText, setSearchText] = useState("");
-
-  const handleTimestampClick = (timestamp: number) => {
-    if (playerRef.current) {
-      playerRef.current.seek(timestamp);
-    }
-  };
-
-  const handleSearchClick = async () => {
-    const isCompressed = file.name.endsWith(".bz2");
-
-    if (isCompressed && result === null) {
-      return;
-    }
-
-    if (isCompressed && result) {
-      const buffer = await result?.arrayBuffer();
-      await search(buffer, searchText);
-    } else {
-      const buffer = await file.arrayBuffer();
-      await search(buffer, searchText);
-    }
-  };
 
   useEffect(() => {
     const initPlayer = async () => {
@@ -140,38 +108,6 @@ const TtyrecPlayer = ({
           ref={containerRef}
           className="bg-black rounded-lg overflow-hidden"
         />
-      )}
-      <div className="flex gap-4 p-2">
-        <div className="flex gap-2">
-          <Input
-            type="text"
-            value={searchText}
-            disabled={
-              status === States.DECOMPRESSING ||
-              searchStatus === SearchStates.SEARCHING
-            }
-            onChange={(e) => setSearchText(e.target.value)}
-          />
-          <Button className="hover:cursor-pointer" onClick={handleSearchClick}>
-            검색
-          </Button>
-        </div>
-      </div>
-      {searchResult && searchResult.length > 0 && (
-        <div className="flex flex-col gap-2 text-start">
-          {searchResult.map((result) => (
-            <div
-              className="flex gap-2 border hover:bg-gray-100"
-              key={result.frame}
-              onClick={() => {
-                handleTimestampClick(result.relativeTimestamp.time);
-              }}
-            >
-              <div>{result.relativeTimestamp.time}</div>
-              <div>{result.textSnippet}</div>
-            </div>
-          ))}
-        </div>
       )}
     </div>
   );

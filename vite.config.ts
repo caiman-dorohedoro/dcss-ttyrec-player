d0afe22ad654f9228e401b3de8e7e542fb185856
caiman <dorohecaimandoro@gmail.com>
2025-03-22 00:51:29 +0900

    fix: delete unnecessary dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import path from 'node:path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import type { PluginOption } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()] as PluginOption[],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
